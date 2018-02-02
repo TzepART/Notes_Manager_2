5c8df75a1e1d0a39f1c8ee5af1ccae61eb03deb9
tzepart <tzepart@gmail.com>
2018-02-02 18:46:55 +0300

    w_add_fos_user_and_migration_bundle

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: artem
+ * Date: 02/02/2018
+ * Time: 18:18
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
