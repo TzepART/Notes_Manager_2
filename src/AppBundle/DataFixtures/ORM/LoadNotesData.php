ce571927931f2bd1b8f253b970450a825bd98fc6
tzepart <tzepart@gmail.com>
2018-02-03 23:39:51 +0300

    w_add_LoadNotesData_and_LoadUsersData

---
+++ src/AppBundle/DataFixtures/ORM/LoadNotesData.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use AppBundle\Entity\Note;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Faker\Factory as FakerFactory;
+use Faker\Generator;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+
+class LoadNotesData extends Fixture implements OrderedFixtureInterface, ContainerAwareInterface
+{
+    const REFERENCE_PREFIX = 'app_note_';
+
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * @var Generator
+     */
+    private $faker;
+
+
+
+    /**
+     * Sets the container.
+     *
+     * @param ContainerInterface|null $container A ContainerInterface instance or null
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+        $this->faker = FakerFactory::create();
+    }
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+
+        for ($i = 0; $i < 80; $i++) {
+            $note = new Note();
+            $userNumber = $i%7;
+
+            $note->setTitle($this->faker->word)
+                ->setText($this->faker->text(200))
+                ->setUser($this->getReference(LoadUsersData::REFERENCE_PREFIX . $userNumber));
+
+            $manager->persist($note);
+
+            if ($this->referenceRepository) {
+                $this->addReference(self::REFERENCE_PREFIX . $i, $note);
+            }
+
+            $manager->flush();
+        }
+    }
+
+    /**
+     * Get the order of this fixture
+     *
+     * @return integer
+     */
+    public function getOrder()
+    {
+        return 2;
+    }
+}
---
+++ src/AppBundle/DataFixtures/ORM/LoadUsersData.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use AppBundle\Entity\User;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+
+class LoadUsersData extends Fixture implements OrderedFixtureInterface, ContainerAwareInterface
+{
+    const REFERENCE_PREFIX = 'app_user_';
+
+    const DATA_LIST = [
+        [
+            'email' => 'user01@mail.com',
+            'username' => 'user0com1mal',
+            'password' => 'qwe123',
+            'roles' => [],
+        ],
+        [
+            'email' => 'user02@mail.com',
+            'username' => 'user0com2mal',
+            'password' => 'qwe123',
+            'roles' => [],
+        ],
+        [
+            'email' => 'user03@mail.com',
+            'username' => 'user0com3mal',
+            'password' => 'qwe123',
+            'roles' => [],
+        ],
+        [
+            'email' => 'user04@mail.com',
+            'username' => 'user0com4mal',
+            'password' => 'qwe123',
+            'roles' => [],
+        ],
+        [
+            'email' => 'user05@mail.com',
+            'username' => 'user0com5mal',
+            'password' => 'qwe123',
+            'roles' => [],
+        ],
+        [
+            'email' => 'user06@mail.com',
+            'username' => 'user0com6mal',
+            'password' => 'qwe123',
+            'roles' => [],
+        ],
+        [
+            'email' => 'user07@mail.com',
+            'username' => 'user0com7mal',
+            'password' => 'qwe123',
+            'roles' => [],
+        ],
+        [
+            'email' => 'user08@mail.com',
+            'username' => 'user0com8mal',
+            'password' => 'qwe123',
+            'roles' => [],
+        ],
+    ];
+
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+
+    /**
+     * Sets the container.
+     *
+     * @param ContainerInterface|null $container A ContainerInterface instance or null
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+
+        foreach (self::DATA_LIST as $i => $data) {
+            $user = new User();
+            $pass = $this->container->get('security.password_encoder')->encodePassword($user, $data['password']);
+
+            $user
+                ->setUsername($data['username'])
+                ->setEmail($data['email'])
+                ->setPassword($pass)
+                ->setEnabled(true)
+                ->setRoles($data['roles']);
+
+            $manager->persist($user);
+
+            if ($this->referenceRepository) {
+                $this->addReference(self::REFERENCE_PREFIX . $i, $user);
+            }
+
+            $manager->flush();
+        }
+    }
+
+    /**
+     * Get the order of this fixture
+     *
+     * @return integer
+     */
+    public function getOrder()
+    {
+        return 1;
+    }
+}
