8d228cb6c839804872c426981add1664302cf280
tzepart <tzepart@gmail.com>
2018-02-08 01:02:26 +0300

    w_fix_js

--- frontend/app/scripts/notes.js
+++ frontend/app/scripts/notes.js
@@ -1,7 +1,7 @@
 function getNoteContent(noteId) {
   var host = "http://" + window.location.hostname + ':' + window.location.port;
   $.ajax({
-    url: isLocalDev ? host + "/note/api/data.json" : host + "/note/api/"+noteId+"/",
+    url: typeof isLocalDev !== "undefined" ? host + "/note/api/data.json" : host + "/note/api/"+noteId+"/",
     type: 'get',
     success:   function (data) {
       document.getElementById('note_view_title').innerHTML = data.title;
